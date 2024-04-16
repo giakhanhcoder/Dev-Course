37b85b55853ddec4667057c6b59644838b95da18
GIA KHANH <giakhanh27403@gmail.com>
2024-04-16 22:32:05 +0700

    first commit

---
+++ src/main/java/com/develop/devcourse/DevcourseApplication.java
@@ -0,0 +1,13 @@
+package com.develop.devcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/devcourse/DevcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.devcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
