65d3e91eaea48fff865d364b8bcc85a103ba84ba
GIA KHANH <giakhanh27403@gmail.com>
2024-05-27 23:37:44 +0700

    update maven.yml

--- src/main/java/com/develop/devcourse/DevcourseApplication.java
+++ src/main/java/com/develop/devcourse/DevcourseApplication.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DevcourseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevcourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevcourseApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper(){return new ModelMapper();};
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
