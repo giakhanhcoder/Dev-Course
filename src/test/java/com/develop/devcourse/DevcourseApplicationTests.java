a898258c782adec37e878100a897a71ba40cdab5
GIA KHANH <giakhanh27403@gmail.com>
2024-05-28 00:13:51 +0700

    update testing

--- src/test/java/com/develop/devcourse/DevcourseApplicationTests.java
+++ src/test/java/com/develop/devcourse/DevcourseApplicationTests.java
@@ -2,7 +2,6 @@ package com.develop.devcourse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class DevcourseApplicationTests {
 
